6525cdffa7d5c57e0a15f2c1c7651e97c0bcec61
Ashmeet Kaur <kau15556@sheridancollege.ca>
2024-02-20 21:14:11 -0500

    fourth commit flavour

--- src/app/flavour/flavour.component.ts
+++ src/app/flavour/flavour.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-flavour',
   templateUrl: './flavour.component.html',
-  styleUrl: './flavour.component.css'
+  styleUrls: ['./flavour.component.css']
 })
-export class FlavourComponent {
+export class FlavourComponent implements OnInit {
+  quantity!: number;
+  selectedFlavour: string = '';
+  subtotal!: number;
 
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const quantityParam = this.route.snapshot.paramMap.get('quantity');
+    if (quantityParam) {
+      this.quantity = +quantityParam;
+    } else {
+
+      this.router.navigate(['/']);
+    }
+    this.calculateSubtotal();
+  }
+
+  calculateSubtotal() {
+    this.subtotal = this.quantity * 2; // $2 per cupcake
+  }
+
+  cancel() {
+    this.router.navigate(['/']);
+  }
+
+
+
+  goNext() {
+    this.router.navigate(['/order']);
+  }
 }
